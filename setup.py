60fadede01ccd0a48eff2fb03920f271474c236e
Aater Suleman <aater@flux7.com>
2014-01-01 23:56:30 -0600

    jamoora version seems to be working and so is vertualenv

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    setup_requires=['pbr>=0.5.19'],
+    pbr=True,
+)
+
--- virtualenv/setup.sh
+++ virtualenv/setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash 
 
-export VIRTUALENVWRAPPER_HOOK_DIR=$JAMOORA/virtualenv/hooks
+mkaws () { 
+    local SAVE=$VIRTUALENVWRAPPER_HOOK_DIR
+    export VIRTUALENVWRAPPER_HOOK_DIR=$JAMOORA/virtualenv/hooks
+    export JAMOORA_PROFILES=$WORKON_HOME
+    mkvirtualenv --no-pip --no-setuptools $1
+    export VIRTUALENVWRAPPER_HOOK_DIR=$SAVE
+    cp -r $JAMOORA/virtualenv/hooks/* $VIRTUAL_ENV/bin/.
+}
+
+mkaws $1
