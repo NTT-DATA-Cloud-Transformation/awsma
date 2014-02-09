c68282bcc09e900bc9a5b8a57cd9b46e5ded577d
Aater Suleman <suleman@aaters-air.westell.com>
2014-02-09 12:54:04 -0600

    put the jq filter for generate ssh inside the .sh script

--- awsma/mkaws_generate_ssh_config.sh
+++ awsma/mkaws_generate_ssh_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkaws_generate_ssh_config () {
-    local BASTION=$1
-    local USERNAME=$2
+    local USERNAME=$1
+    local BASTION=$2
 
     local VENV=$(basename $VIRTUAL_ENV)
     
@@ -10,7 +10,26 @@ mkaws_generate_ssh_config () {
 DISCLAIMER: This config is a best effort. Every setup is different
 so validate the config file after it has been created'
     
-    aws ec2 describe-instances | jq -r --arg bastion $BASTION --arg username $USERNAME -f generate_ssh_config_filter.jq > $VIRTUAL_ENV/ssh-config
+    aws ec2 describe-instances | jq -r --arg bastion $BASTION --arg username $USERNAME '
+.Reservations[].Instances[] 
+| 
+select(.State.Code == 16) 
+| 
+{
+  KeyName, 
+  name: .Tags[0].Value,
+  PrivateIpAddress,
+  PublicIpAddress
+}
+| 
+( 
+"Host " + .name + "\n" 
++ "HostName " + if .PublicIpAddress then .PublicIpAddress else .PrivateIpAddress end + "\n" 
++ "User " +  if $username then $username else "ubuntu" end + "\n"  
+#+ "IdentityFile " + "\"" + if $identities then $identities else "~/.ssh" end + .KeyName + ".pem\"\n" 
++ if .PublicIpAddress then "" else "ProxyCommand ssh " + $bastion + " exec nc %h %p" + "\n" end
+)
+' > $VIRTUAL_ENV/ssh-config
     
 #    deactivate 
 #    workon $VENV 
